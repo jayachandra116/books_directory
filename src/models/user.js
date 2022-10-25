aa6eee61826d46268bc9e7f68b542f79ba7707a0
Jaya Chandra <mjchandra97@gmail.com>
2022-10-25 19:38:07 +0530

    Pwd to JWT token

--- src/models/user.js
+++ src/models/user.js
@@ -1,42 +1,48 @@
-const mogoose=require('mongoose');
-const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken');
-const validator=require('validator');
+const mogoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
 
-const userSchema=new mogoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        unique:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid');
-            }
+const userSchema = new mogoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
         }
+      },
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:8,
-        trim:true,
-        validate(value){
-            //Minimum eight characters, at least one letter and one number:
-            //if(!value.match("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")){
-            if(value.length<8){
-                throw new Error("Pwd must be having length greater than 8")
-            } 
-        }
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      trim: true,
     },
-    profileName:{
-        type:String,
-        trim:true
+    profileName: {
+      type: String,
+      trim: true,
     },
-    
-},{
-    timestamps:true
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User=mogoose.model('User',userSchema)
-module.exports=User;
+// Hash the plain text password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mogoose.model("User", userSchema);
+module.exports = User;
